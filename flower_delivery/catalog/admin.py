a8a075ec090235b77e2b134c712d16f1d32a5e80
Alexandr-sys2024 <time2016slot@gmail.com>
2025-02-03 11:31:18 +0300

    add new code in admin in catalog

--- flower_delivery/catalog/admin.py
+++ flower_delivery/catalog/admin.py
@@ -1,3 +1,20 @@
+from django.utils.html import format_html
 from django.contrib import admin
+from .models import Flower
 
-# Register your models here.
+@admin.register(Flower)
+class FlowerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'image_preview')  # Добавляем превью картинки
+    fields = ('name', 'description', 'price', 'image', 'image_preview')  # Добавляем метод
+    readonly_fields = ('image_preview',)  # Чтобы поле не редактировалось
+
+    list_filter = ('created_at',)
+    list_per_page = 10
+
+    def image_preview(self, obj):
+        """ Показывает превью загруженного изображения в админке """
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 100px;"/>', obj.image.url)
+        return "(Нет изображения)"
+
+    image_preview.short_description = "Превью"
