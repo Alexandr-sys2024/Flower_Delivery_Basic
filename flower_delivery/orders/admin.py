cf582a270c04f634b8f2b80e0fd8bc1be984c867
Alexandr-sys2024 <time2016slot@gmail.com>
2025-02-03 11:32:47 +0300

    add code in admin in orders

--- flower_delivery/orders/admin.py
+++ flower_delivery/orders/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Cart, CartItem
 
-# Register your models here.
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'status', 'created_at', 'updated_at')  # Заменили 'user' на 'user_id'
+    list_filter = ('status', 'created_at')
+    inlines = [CartItemInline]
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cart', 'flower', 'quantity', 'price')
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'status', 'order_key', 'created_at', 'total_price')  # Исправили 'user' на 'user_id'
+    list_filter = ('status', 'created_at')
+    search_fields = ('order_key',)  # Убрали 'user__username', т.к. user_id — это просто число
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'flower', 'quantity', 'price')
